8ed8febd87aff24574adc9a8b7acbd13bd27aa80
nqtran1 <nqtran@ucsd.edu>
2021-02-18 10:49:58 -0800

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(clickLikeBtn);
+}
+function clickLikeBtn(){
+	e.preventDefault();
+	ga('create', 'UA-189464502-2', 'auto');
+	ga('send','event','signup','click');
 }
